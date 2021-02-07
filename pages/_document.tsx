a3b1aa766ce55eeba7bc781f91a8ad0677d5cb95
Umar Luqman <>
2021-02-07 21:41:16 +0800

    Added favicons, and plausible

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,42 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head />
+        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/assets/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/assets/safari-pinned-tab.svg"
+          color="#3391ab"
+        />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
+
         <script src="https://cdn.paritybar.com/paritybar.js"></script>
+        <script
+          async
+          defer
+          data-domain="focusmode.app"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
+
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
