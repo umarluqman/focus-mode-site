f3121935662286a8f97c99c8af6a56285fec442a
Umar Luqman <>
2021-02-10 11:33:25 +0800

    Remove unwanted faq

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Flex, Text, chakra } from "@chakra-ui/react";
+import { Box, Grid, Flex, Text, chakra, Link } from "@chakra-ui/react";
 import React from "react";
 
 const Faq = () => {
@@ -57,32 +57,13 @@ const Faq = () => {
           <Box borderTop="1px solid #2A4257" pt={3} mb={12}>
             <Grid templateColumns={{ base: "1fr", md: "20rem 1fr" }} gap={8}>
               <Text fontSize="lg" color="white" pb={3}>
-                What if I want to take a break?
+                I have a question but it's not here
               </Text>
               <Text color="#C8F2FF" lineHeight={2}>
-                You can just take a 5 minutes break whenver you want or turn off
-                the focus mode for good.
-              </Text>
-            </Grid>
-          </Box>
-          <Box borderTop="1px solid #2A4257" pt={3} mb={12}>
-            <Grid templateColumns={{ base: "1fr", md: "20rem 1fr" }} gap={8}>
-              <Text fontSize="lg" color="white" pb={3}>
-                How is this going to be distributed after I purchased?
-              </Text>
-              <Text color="#C8F2FF" lineHeight={2}>
-                After purchased, you will get an email with a link, just
-                download and open it. It will be installed automatically.
-              </Text>
-            </Grid>
-          </Box>
-          <Box borderTop="1px solid #2A4257" pt={3} mb={12}>
-            <Grid templateColumns={{ base: "1fr", md: "20rem 1fr" }} gap={8}>
-              <Text fontSize="lg" color="white" pb={3}>
-                If I buy now, do I get updates?
-              </Text>
-              <Text color="#C8F2FF" lineHeight={2}>
-                It's a lifetime deal, purchase once and upgrade forever.
+                Just hit me up at{" "}
+                <Link isExternal color="white">
+                  https://twitter.com/umarlqmn
+                </Link>
               </Text>
             </Grid>
           </Box>
